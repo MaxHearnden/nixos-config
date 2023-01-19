4612f8f4c2501fbf91f891b3a6dc5aec4cb1311a
MaxHearnden <maxoscarhearnden@gmail.com>
2023-01-19 21:56:51 +0000

    make trusted keys a list

--- remote.nix
+++ remote.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }: {
   imports = [./configuration.nix];
   nix.settings = {
-    trusted-public-keys = "max-nixos-workstation:Ds5AWfGPm6jRbVSjG4ht42MK++hhfFczQ4bJRhD9thI=";
+    trusted-public-keys = ["max-nixos-workstation:Ds5AWfGPm6jRbVSjG4ht42MK++hhfFczQ4bJRhD9thI="];
     substituters = ["http://172.28.10.244:8080"];
   };
   system.autoUpgrade.flake = "git+http://172.28.10.244:3000/zandoodle/nixos-config";
