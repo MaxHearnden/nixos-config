08cabfb90073288a27f5d73e06f076e08b43439f
MaxHearnden <maxoscarhearnden@gmail.com>
2023-01-19 21:53:38 +0000

    don't include workstation hardware in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,10 +6,10 @@
 
 {
   hardware.enableAllFirmware = true;
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+#  imports =
+#    [ # Include the results of the hardware scan.
+#      ./hardware-configuration.nix
+#    ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.grub = {
