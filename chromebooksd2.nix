cfde1f3177b4dde6a6b244724b5c4442d9a4513e
MaxHearnden <maxoscarhearnden@gmail.com>
2023-01-20 01:22:19 +0000

    use the hardware configutation

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -1,4 +1,4 @@
 { config, pkgs, lib, ... }: {
-  imports = [ ./remote.nix ];
+  imports = [ ./remote.nix ./hardware-configuration/chromebooksd2.nix ];
   networking.hostName = "max-nixos-chromebooksd2";
 }
