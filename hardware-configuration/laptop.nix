484604893f14558949b64f4a5f9d8412adc384d5
MaxHearnden <maxoscarhearnden@gmail.com>
2023-01-20 13:43:22 +0000

    prepare for btrfs

--- hardware-configuration/laptop.nix
+++ hardware-configuration/laptop.nix
@@ -15,7 +15,7 @@
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/8cc18f77-70e3-4860-854d-5d668c4473d3";
-      fsType = "ext4";
+      fsType = "btrfs";
     };
 
   fileSystems."/boot/efi" =
