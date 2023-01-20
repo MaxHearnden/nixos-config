ea6a7cb6d0d88ac52a5be56e445697ac50b37c36
MaxHearnden <maxoscarhearnden@gmail.com>
2023-01-20 12:06:48 +0000

    add swapfile

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -1,4 +1,5 @@
 { config, pkgs, lib, ... }: {
   imports = [ ./remote.nix ./hardware-configuration/chromebooksd2.nix ./laptop.nix ];
   networking.hostName = "max-nixos-chromebooksd2";
+  swapDevices = [ {device = "/swapfile"; size = 4096;} ];
 }
