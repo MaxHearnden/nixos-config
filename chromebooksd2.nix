0afe4d597f8a25ea2d8088d7ffa7f8aa873d32c8
MaxHearnden <maxoscarhearnden@gmail.com>
2023-01-25 21:49:28 +0000

    readd installing grub as removable

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -2,4 +2,6 @@
   imports = [ ./remote.nix ./hardware-configuration/chromebooksd2.nix ./laptop.nix ];
   networking.hostName = "max-nixos-chromebooksd2";
   swapDevices = [ {device = "/swapfile"; size = 4096;} ];
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
 }
