549356ff1bc86703ae85841fe876541b9d617000
zandoodle <zandoodle@noreply.172.28.10.244>
2023-01-25 21:09:33 +0000

    revert 2ad6a8c27789176e7e6e8a496a15cd831a35317f
    revert override option

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -3,5 +3,4 @@
   networking.hostName = "max-nixos-chromebooksd2";
   swapDevices = [ {device = "/swapfile"; size = 4096;} ];
   boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
 }
