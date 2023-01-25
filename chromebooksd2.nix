2ad6a8c27789176e7e6e8a496a15cd831a35317f
zandoodle <zandoodle@noreply.172.28.10.244>
2023-01-25 20:43:40 +0000

    override option

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -3,4 +3,5 @@
   networking.hostName = "max-nixos-chromebooksd2";
   swapDevices = [ {device = "/swapfile"; size = 4096;} ];
   boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
 }
