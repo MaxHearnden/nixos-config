5a3fa50cb6681a3f2451658a900b4812b5999850
zandoodle <zandoodle@noreply.172.28.10.244>
2023-02-22 17:31:56 +0000

    remove remote building
    It wasn't faster

--- remote.nix
+++ remote.nix
@@ -4,19 +4,19 @@
     trusted-public-keys = ["max-nixos-workstation:Ds5AWfGPm6jRbVSjG4ht42MK++hhfFczQ4bJRhD9thI="];
     substituters = ["http://172.28.10.244:8080"];
   };
-  nix.buildMachines = [
-    {
-      systems = [ "riscv64-linux" "mips-linux" "mipsel-linux" "mips64-linux" "mips64el-linux" "aarch64-linux" "riscv32-linux" "x86_64-linux" "i686-linux" ];
-      sshUser = "root";
-      hostName = "172.28.10.244";
-      maxJobs = 4;
-      supportedFeatures = [ "kvm"
-        "big-parallel"
-        "benchmark"
-        "nixos-test"
-      ];
-    }
-  ];
-  nix.distributedBuilds = true;
+  # nix.buildMachines = [
+  #   {
+  #     systems = [ "riscv64-linux" "mips-linux" "mipsel-linux" "mips64-linux" "mips64el-linux" "aarch64-linux" "riscv32-linux" "x86_64-linux" "i686-linux" ];
+  #     sshUser = "root";
+  #     hostName = "172.28.10.244";
+  #     maxJobs = 4;
+  #     supportedFeatures = [ "kvm"
+  #       "big-parallel"
+  #       "benchmark"
+  #       "nixos-test"
+  #     ];
+  #   }
+  # ];
+  # nix.distributedBuilds = true;
 
 }
