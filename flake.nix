7ebef0518a704ac4af7eba522f02255467acb479
MaxHearnden <maxoscarhearnden@gmail.com>
2023-02-22 00:08:19 +0000

    move Math104 from github to gitea

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   #};
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.math104.url = "github:MaxHearnden/Math104";
+  inputs.math104.url = "git+http://172.28.10.244:3000/zandoodle/Math104";
   inputs.math104.inputs.nixpkgs.follows = "nixpkgs";
   #inputs.nixpkgs-mipsel.url = "github:maxHearnden/nixpkgs/patch-1";
   outputs = { self, nixpkgs, ... }@inputs: {
