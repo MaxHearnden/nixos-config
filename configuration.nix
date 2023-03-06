cf2373b0f3fbd14e1b1adbc69c69a3b95af4dd4d
MaxHearnden <maxoscarhearnden@gmail.com>
2023-03-06 11:36:19 +0000

    update autoconf

--- configuration.nix
+++ configuration.nix
@@ -199,8 +199,8 @@
           {
             name = "autoconf";
             publisher = "maelvalais";
-            version = "0.1.0";
-            sha256 = "sha256-h5K2JTqRXIQBm+fgl0nE31E4lmiXXMGmgtKM6oG1gFc=";
+            version = "0.2.0";
+            sha256 = "sha256-DjRQDSsVThJ6lOkkS4TROyPHQXOchhT6YqWWmeTtihQ=";
           }
           {
             name = "vala";
