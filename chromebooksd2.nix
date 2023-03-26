5b0f8d6e37eafd464d2a527ce3030d61aa8a3ed2
zandoodle <zandoodle@noreply.172.28.10.244>
2023-03-26 13:14:06 +0100

    remove size of swapfile
    It would only cause it to fail

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }: {
   imports = [ ./remote.nix ./hardware-configuration/chromebooksd2.nix ./laptop.nix ];
   networking.hostName = "max-nixos-chromebooksd2";
-  swapDevices = [ {device = "/swapfile"; size = 4096;} ];
+  swapDevices = [ {device = "/swapfile";} ];
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
   system.autoUpgrade.enable = lib.mkForce false;
