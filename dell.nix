7c49316828862e25fe39285db65d1e7d24b718bd
zandoodle <zandoodle@noreply.172.28.10.244>
2023-03-26 15:11:35 +0100

    Add result of memtest

--- dell.nix
+++ dell.nix
@@ -1,4 +1,5 @@
 { config, pkgs, lib, ... }: {
   imports = [./remote.nix ./hardware-configuration/dell.nix ./laptop.nix];
   networking.hostName = "max-nixos-dell";
+  boot.kernelParams = ["badmem=0x0000000099a09810,0xfffffffffffffff8"]
 }
