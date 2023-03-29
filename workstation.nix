2a374d43d0e64c255dea3209a425d29afa137f63
zandoodle <zandoodle@noreply.172.28.10.244>
2023-03-30 00:10:13 +0100

    correct default options

--- workstation.nix
+++ workstation.nix
@@ -76,7 +76,7 @@
     enable = true;
     hostName = "172.28.10.244";
     exports = ''
-      /Big/shared -(mp=/Big,rw,all_squash,anonuid=1000,anongid=100,async) max-nixos-* max-guix-*
+      /Big/shared -mp=/Big,rw,all_squash,anonuid=1000,anongid=100,async max-nixos-* max-guix-*
     '';
   };
   services.kerberos_server = {
