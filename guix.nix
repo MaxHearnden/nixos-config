22a5cb71bf3f49a9c44b3622847667c3d9f18e0d
MaxHearnden <maxoscarhearnden@gmail.com>
2023-03-29 11:45:51 +0100

    Add menu entry for guix

---
+++ guix.nix
@@ -0,0 +1,11 @@
+# add grub entries for a guix system
+{
+  boot.loader.grub.extraEntries = [
+    ''
+      menuentry "Guix" {
+        search --fs-uuid --set F5DE-4023
+        chainloader /EFI/Guix/grubx64.efi
+      }
+    ''
+  ];
+}
