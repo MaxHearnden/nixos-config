95a8d36187ee16a2f7129de0a46edb09a1a27dc6
MaxHearnden <maxoscarhearnden@gmail.com>
2023-06-14 01:55:19 +0100

    rearange dell.nix

--- dell.nix
+++ dell.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }: {
   imports = [./remote.nix ./hardware-configuration/dell.nix ./laptop.nix ./guix.nix];
-  networking.hostName = "max-nixos-dell";
+  networking = {
+    hostName = "max-nixos-dell";
+  };
   #boot.loader.grub.extraConfig = "badram 0x0000000099a09810,0xfffffffffffffff8";
 }
