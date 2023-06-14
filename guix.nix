4d093044fffa05e48197f47a0e08a7737d0fa012
MaxHearnden <maxoscarhearnden@gmail.com>
2023-06-14 01:56:35 +0100

    rearange guix.nix

--- guix.nix
+++ guix.nix
@@ -1,9 +1,15 @@
 # add grub entries for a guix system
 {
-  boot.loader.grub.extraEntries = ''
-    menuentry "Guix" {
-      search --fs-uuid --set F5DE-4023
-      chainloader /EFI/Guix/grubx64.efi
-    }
-  '';
+  boot = {
+    loader = {
+      grub = {
+        extraEntries = ''
+          menuentry "Guix" {
+            search --fs-uuid --set F5DE-4023
+            chainloader /EFI/Guix/grubx64.efi
+          }
+        '';
+      };
+    };
+  };
 }
