25e1d9b2518b6742e2c2396cec215c824fd69948
MaxHearnden <maxoscarhearnden@gmail.com>
2023-10-08 11:59:15 +0100

    correct shellcheck issues with nixos-upgrade

--- remote.nix
+++ remote.nix
@@ -51,13 +51,13 @@
         restartIfChanged = false;
         script = ''
           config="$(${pkgs.curl}/bin/curl "http://172.28.10.244:8081/${config.networking.hostName}" -f)"
-          nix-env -p /nix/var/nix/profiles/system --set "$config"
+          nix-env -p /nix/var/nix/profiles/system --set "''${config}"
           # booted="$(${pkgs.coreutils}/bin/readlink /run/booted-system/{initrd,kernel,kernel-modules})"
           # built="$(${pkgs.coreutils}/bin/readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
           # if [ "''${booted}" = "''${built}" ]; then
           #   $config/bin/switch-to-configuration switch
           # else
-          $config/bin/switch-to-configuration boot
+          "''${config}/bin/switch-to-configuration" boot
           # fi
         '';
         unitConfig = {
