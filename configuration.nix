e38f3e3a36bc596a89d2cc24115e71f6413e5196
MaxHearnden <maxoscarhearnden@gmail.com>
2023-10-16 10:13:59 +0100

    add ca cache

--- configuration.nix
+++ configuration.nix
@@ -130,8 +130,8 @@
         "nix-command"
         "flakes"
       ];
-      trusted-public-keys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
-      trusted-substituters = [ "https://cache.iog.io" ];
+      trusted-public-keys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA=" ];
+      trusted-substituters = [ "https://cache.ngi0.nixos.org" "https://cache.iog.io" ];
     };
   };
   nixpkgs = {
