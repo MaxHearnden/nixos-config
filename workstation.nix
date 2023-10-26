3a4e6297ed7434df9836529dd8b86241d42b666f
MaxHearnden <maxoscarhearnden@gmail.com>
2023-10-26 15:30:14 +0100

    add nat

--- workstation.nix
+++ workstation.nix
@@ -74,6 +74,13 @@
         useDHCP = false;
       };
     };
+    nat = {
+      enable = true;
+      externalInterface = "eno1";
+      internalInterfaces = [
+        "enp1s0"
+      ];
+    };
     networkmanager = {
       unmanaged = [
         "enp1s0"
