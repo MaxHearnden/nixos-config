822cb5381dc9714d29a4315c3867de262fe944f8
MaxHearnden <maxoscarhearnden@gmail.com>
2023-11-23 22:26:54 +0000

    keep failed derivations

--- configuration.nix
+++ configuration.nix
@@ -135,6 +135,7 @@
         "nix-command"
         "flakes"
       ];
+      keep-failed = true;
       trusted-public-keys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA=" ];
       trusted-substituters = [ "https://cache.ngi0.nixos.org" "https://cache.iog.io" ];
     };
