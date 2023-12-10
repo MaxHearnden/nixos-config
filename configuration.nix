0f4bd50e7d073421f30c1d5bcea6e59184531ab8
MaxHearnden <maxoscarhearnden@gmail.com>
2023-12-10 16:50:58 +0000

    allow access to /dev/kvm

--- configuration.nix
+++ configuration.nix
@@ -389,6 +389,7 @@
           CapabilityBoundingSet = "CAP_SYS_CHROOT CAP_CHOWN CAP_SETUID CAP_SETGID CAP_SYS_ADMIN CAP_DAC_OVERRIDE CAP_DAC_READ_SEARCH CAP_KILL CAP_FOWNER";
           ProtectSystem = "strict";
           BindPaths = "/dev/kvm";
+          DeviceAllow = "/dev/kvm";
           # RootDirectory = "/var/empty";
           # TemporaryFileSystem = "/";
           # BindReadOnlyPaths = "/etc/nix /etc/resolv.conf /etc/ssl /etc/static/ssl /etc/passwd /etc/group /machines";
@@ -399,7 +400,6 @@
           NoNewPrivileges = true;
           PrivateDevices = true;
           ProtectClock = true;
-          ProtectKernelLogs = true;
           ProtectControlGroups = true;
           ProtectKernelModules = true;
           SocketBindDeny = "any";
