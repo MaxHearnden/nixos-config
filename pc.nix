ddc6b5f9d81b8fa43013f8c87a24557c6ee6d474
MaxHearnden <maxoscarhearnden@gmail.com>
2023-12-20 12:26:39 +0000

    add mount point for the root subvolume

--- pc.nix
+++ pc.nix
@@ -29,6 +29,13 @@
       useTmpfs = true;
     };
   };
+  fileSystems = {
+    "/nexus" = {
+      device = "/dev/disk/by-uuid/23d34216-8396-41b9-ae01-290d9fbf1a6d";
+      fsType = "btrfs";
+      options = [ "defaults" "compress=zstd" "nosuid" "nodev" "noatime" ];
+    }
+  };
   networking = {
     hostName = "max-nixos-pc";
   };
