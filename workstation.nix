fd82e26ddb027882da182f5c752fdb3cc09a2c51
MaxHearnden <maxoscarhearnden@gmail.com>
2023-12-22 03:40:26 +0000

    remove restrict-path

--- workstation.nix
+++ workstation.nix
@@ -104,7 +104,6 @@
         {
           key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQQFWFgxHvO9V02h2V7Ylgiou9o745w08xBEddL7HA0 btrbk@max-nixos-pc";
           roles = [
-            "restrict-path /nexus/snapshots/per-device/pc"
             "receive"
           ];
         }
