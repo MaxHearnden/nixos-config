49c6e1060090648193c59b5f2bf2c1a5175dbe38
MaxHearnden <maxoscarhearnden@gmail.com>
2023-12-24 21:09:16 +0000

    fix percentage

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -27,7 +27,7 @@
             target_preserve = "2w 6m";
             ssh_user = "btrbk";
             send_compressed_data = "yes";
-            stream_buffer = "%25";
+            stream_buffer = "25%";
             stream_compress = "zstd";
             snapshot_preserve = "14d 2w 3m";
             snapshot_preserve_min = "2d";
