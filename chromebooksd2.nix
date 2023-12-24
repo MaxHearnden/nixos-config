0f7c4273e219f1cf63b92376d119120ab4dcd75f
MaxHearnden <maxoscarhearnden@gmail.com>
2023-12-24 21:04:47 +0000

    add btrbk to chromebook

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -15,6 +15,35 @@
   networking = {
     hostName = "max-nixos-chromebooksd2";
   };
+  services = {
+    btrbk = {
+      extraPackages = with pkgs; [
+        zstd
+      ];
+      instances = {
+        pc = {
+          settings = {
+            target_preserve_min = "no";
+            target_preserve = "2w 6m";
+            ssh_user = "btrbk";
+            send_compressed_data = "yes";
+            stream_buffer = "%25";
+            stream_compress = "zstd";
+            snapshot_preserve = "14d 2w 3m";
+            snapshot_preserve_min = "2d";
+            snapshot_dir = "snapshots/btrbk";
+            transaction_syslog = "user";
+            target = "ssh://172.28.10.244/Big/backups/chromebooksd2";
+            volume = {
+              "/nexus" = {
+                subvolume = "@NixOS";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
   swapDevices = [
     {
       device = "/swapfile";
