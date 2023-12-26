ce4aba6e0eef6ba0366bd62b957af82786ed552c
MaxHearnden <maxoscarhearnden@gmail.com>
2023-12-26 16:10:21 +0000

    add dhcp to chromebook

--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -14,10 +14,56 @@
   imports = [ ./remote.nix ./hardware-configuration/chromebooksd2.nix ./laptop.nix ];
   networking = {
     hostName = "max-nixos-chromebooksd2";
+    hosts =
+      lib.listToAttrs (
+        lib.genList (index:
+          lib.nameValuePair "192.168.2.1${toString (index + 1)}" [ "nixos-slot${toString (index + 1)}"]
+        ) 7
+      );
+    interfaces = {
+      net-dhcp = {
+        ipv4 = {
+          addresses = [
+            {
+              address = "192.168.2.1";
+              prefixLength = 24;
+            }
+          ];
+        };
+        useDHCP = false;
+      };
+    };
+    nat = {
+      enable = true;
+      externalInterface = "wlp2s0";
+      internalInterfaces = [
+        "net-dhcp"
+      ];
+    };
+    networkmanager = {
+      unmanaged = [
+        "net-dhcp"
+      ];
+    };
   };
   swapDevices = [
     {
       device = "/nexus/swapfile";
     }
   ];
+  systemd = {
+    network = {
+      links = {
+        "10-net-dhcp" = {
+          linkConfig = {
+            Name = "net-dhcp";
+            NamePolicy = "";
+          };
+          matchConfig = {
+            MACAddress = "9c:eb:e8:0f:91:63";
+          };
+        };
+      };
+    };
+  };
 }
