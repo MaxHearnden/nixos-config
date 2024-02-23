1898f01bcfb6f8cd7da430deec2c141d6c545a5d
MaxHearnden <maxoscarhearnden@gmail.com>
2024-02-23 15:47:53 +0000

    print capabilities

--- remote.nix
+++ remote.nix
@@ -145,9 +145,10 @@
         requires = [ "network-online.target" "zerotierone.service" "blkid-cache.service" ];
         restartIfChanged = false;
         script = ''
+          ${pkgs.libcap}/bin/capsh --print
           config="$(${pkgs.curl}/bin/curl "http://172.28.10.244:8081/${config.networking.hostName}" -f)"
           ${config.nix.package}/bin/nix-env -p /nix/var/nix/profiles/system --set "''${config}"
-          ${pkgs.strace}/bin/strace -Z -f "''${config}/bin/switch-to-configuration" boot
+          "''${config}/bin/switch-to-configuration" boot
         '';
         unitConfig = {
           X-StopOnRemoval = false;
