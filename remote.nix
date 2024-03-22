cac38defcafd8cda96a4c9767ac21fe6ee539eb9
MaxHearnden <maxoscarhearnden@gmail.com>
2024-03-22 04:19:07 +0000

    allow btrbk to use ipv6

--- remote.nix
+++ remote.nix
@@ -125,6 +125,7 @@
           IPAddressAllow = "172.28.10.244 fd80:56c2:e21c:3d4b:0c99:93c5:0d88:e258 fc9c:6b89:eec5:0d88:e258:0000:0000:0001";
           RestrictSUIDSGID = true;
           RestrictAddressFamilies = "AF_INET AF_INET6 AF_UNIX";
+          BindReadOnlyPaths = "/run/nscd";
         };
       };
       "nixos-upgrade" = {
