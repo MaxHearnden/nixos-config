4bc70bc2f389148d45d34a21c9470457dffb99af
MaxHearnden <maxoscarhearnden@gmail.com>
2024-03-28 00:45:14 +0000

    add pkgs.

--- configuration.nix
+++ configuration.nix
@@ -424,8 +424,8 @@
   system = {
     extraDependencies = [
       # Prevent unnecicary gc and rebuild
-      pkgsCross.riscv32.buildPackages.gcc.info
-      pkgsCross.riscv64.buildPackages.gcc.info
+      pkgs.pkgsCross.riscv32.buildPackages.gcc.info
+      pkgs.pkgsCross.riscv64.buildPackages.gcc.info
     ];
     stateVersion = "23.05";
   };
