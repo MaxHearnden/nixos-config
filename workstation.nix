7b16e1ba70051bb14eed676da47728f7a11c002c
MaxHearnden <maxoscarhearnden@gmail.com>
2024-08-29 15:17:13 +0100

    allow nfs server to be accessed from any port

--- workstation.nix
+++ workstation.nix
@@ -337,7 +337,7 @@
         enable = true;
         hostName = "max-nixos-workstation-zerotier-ipv4,max-nixos-workstation-zerotier-6plane,max-nixos-workstation-zerotier-rfc4193,192.168.2.1";
         exports = ''
-          /Big/shared -mp=/Big,rw,all_squash,anonuid=1000,anongid=100,async max-nixos-* max-guix-*
+          /Big/shared -mp=/Big,rw,all_squash,anonuid=1000,anongid=100,async,insecure max-nixos-* max-guix-*
           /Big/shared/riscv/star64_root 192.168.2.0/24(rw,no_root_squash,mp=/Big)
           /nix 192.168.2.0/24(ro,no_root_squash)
         '';
