f057a8a2e811b7a8fdee3db1fb35427978398a8a
MaxHearnden <maxoscarhearnden@gmail.com>
2024-11-04 17:12:08 +0000

    move OnSuccess

--- remote.nix
+++ remote.nix
@@ -136,7 +136,6 @@
           IPAddressAllow = "172.28.10.244 fd80:56c2:e21c:3d4b:0c99:93c5:0d88:e258 fc9c:6b89:eec5:0d88:e258:0000:0000:0001";
           ProtectProc = "invisible";
           MemoryDenyWriteExecute = true;
-          OnSuccess = "nixos-upgrade-apply.service";
         };
         path = with pkgs; [
           config.nix.package.out
@@ -150,6 +149,7 @@
         '';
         unitConfig = {
           X-StopOnRemoval = false;
+          OnSuccess = "nixos-upgrade-apply.service";
         };
       };
       nixos-upgrade-apply = {
