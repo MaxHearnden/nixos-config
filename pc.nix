555d2478c8a8592ba0a157bbe07150a87fd79d00
MaxHearnden <maxoscarhearnden@gmail.com>
2024-12-29 17:14:34 +0000

    bind bound /HDD/backups in btrfs service

--- pc.nix
+++ pc.nix
@@ -536,6 +536,7 @@
       };
       btrbk-btrbk = {
         serviceConfig = {
+          BindPaths = [ "/HDD/backups" ];
           PrivateNetwork = lib.mkForce false;
           IPAddressAllow = "172.28.10.244 fd80:56c2:e21c:3d4b:0c99:93c5:0d88:e258 fc9c:6b89:eec5:0d88:e258:0000:0000:0001";
           RestrictSUIDSGID = lib.mkForce false;
