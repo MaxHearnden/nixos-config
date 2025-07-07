e75ff11c11797796c831c67be51638ea06bb06d6
MaxHearnden <maxoscarhearnden@gmail.com>
2025-07-07 20:12:34 +0100

    Use orion for DNS on pc

--- pc.nix
+++ pc.nix
@@ -177,78 +177,22 @@
     ratbagd = {
       enable = true;
     };
-    unbound.settings.auth-zone = [
-      {
-        for-downstream = "no";
+    unbound.settings = {
+      forward-zone = {
         name = ".";
-        primary = [
-          "199.9.14.201"         # b.root-servers.net
-          "192.33.4.12"          # c.root-servers.net
-          "199.7.91.13"          # d.root-servers.net
-          "192.5.5.241"          # f.root-servers.net
-          "192.112.36.4"         # g.root-servers.net
-          "193.0.14.129"         # k.root-servers.net
-          "192.0.47.132"         # xfr.cjr.dns.icann.org
-          "192.0.32.132"         # xfr.lax.dns.icann.org
-          "2001:500:200::b"      # b.root-servers.net
-          "2001:500:2::c"        # c.root-servers.net
-          "2001:500:2d::d"       # d.root-servers.net
-          "2001:500:2f::f"       # f.root-servers.net
-          "2001:500:12::d0d"     # g.root-servers.net
-          "2001:7fd::1"          # k.root-servers.net
-          "2620:0:2830:202::132" # xfr.cjr.dns.icann.org
-          "2620:0:2d0:202::132"  # xfr.lax.dns.icann.org
-        ];
-        url = "https://www.internic.net/domain/root.zone";
-        fallback-enabled = true;
-        zonemd-check = true;
-        zonefile = "/var/lib/unbound/root.zone";
-      }
-      {
-        for-downstream = "no";
-        name = "arpa";
-        primary = [
-          "199.9.14.201"         # b.root-servers.net
-          "192.33.4.12"          # c.root-servers.net
-          "199.7.91.13"          # d.root-servers.net
-          "192.5.5.241"          # f.root-servers.net
-          "192.112.36.4"         # g.root-servers.net
-          "193.0.14.129"         # k.root-servers.net
-          "192.0.47.132"         # xfr.cjr.dns.icann.org
-          "192.0.32.132"         # xfr.lax.dns.icann.org
-          "2001:500:200::b"      # b.root-servers.net
-          "2001:500:2::c"        # c.root-servers.net
-          "2001:500:2d::d"       # d.root-servers.net
-          "2001:500:2f::f"       # f.root-servers.net
-          "2001:500:12::d0d"     # g.root-servers.net
-          "2001:7fd::1"          # k.root-servers.net
-          "2620:0:2830:202::132" # xfr.cjr.dns.icann.org
-          "2620:0:2d0:202::132"  # xfr.lax.dns.icann.org
-        ];
-        url = "https://www.internic.net/domain/arpa.zone";
-        fallback-enabled = true;
-        zonemd-check = true;
-        zonefile = "/var/lib/unbound/arpa.zone";
-      }
-      {
-        for-downstream = "no";
-        name = "in-addr.arpa";
-        url = "https://www.internic.net/domain/in-addr.arpa.zone";
-        zonefile = "/var/lib/unbound/in-addr.arpa.zone";
-      }
-      {
-        for-downstream = "no";
-        name = "ip6.arpa";
-        url = "https://www.internic.net/domain/ip6.arpa.zone";
-        zonefile = "/var/lib/unbound/ip6.arpa.zone";
-      }
-      {
-        for-downstream = "no";
-        name = "root-servers.net";
-        url = "https://www.internic.net/domain/root-servers.net.zone";
-        zonefile = "/var/lib/unbound/root-servers.net.zone";
-      }
-    ];
+        forward-host = [ "orion.broadband@55" ];
+      };
+      server = {
+        domain-insecure = [ "broadband" ];
+        qname-minimisation = false;
+      };
+      stub-zone = [
+        {
+          name = "broadband";
+          stub-addr = "192.168.1.1";
+        }
+      ];
+    };
     xserver = {
       displayManager.gdm.autoSuspend = false;
       videoDrivers = [
