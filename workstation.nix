25c49adc08f2dfae83e9dfd0219e8f46cb944c4b
MaxHearnden <maxoscarhearnden@gmail.com>
2026-02-03 11:39:09 +0000

    Enable compression for cache.workstation.zandoodle.me.uk

--- workstation.nix
+++ workstation.nix
@@ -321,6 +321,7 @@ in
 
           @harmonia host cache.workstation.zandoodle.me.uk
           handle @harmonia {
+            encode
             header {
               Content-Security-Policy "base-uri 'none'; default-src 'none'; form-action 'none'; frame-ancestors 'none'; script-src 'sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4'; style-src-elem https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css;"
             }
@@ -420,6 +421,7 @@ in
       signKeyPaths = ["/etc/nix/storekey"];
       settings = {
         bind = "[::1]:8080";
+        enable_compression = true;
         priority = 50;
       };
     };
