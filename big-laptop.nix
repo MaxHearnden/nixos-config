86a9c0b9bc0bc951f4881bb894e89e5e3d6dcaed
MaxHearnden <maxoscarhearnden@gmail.com>
2026-05-20 19:14:00 +0100

    Add build farm as remote builder for laptop and chromebook

--- big-laptop.nix
+++ big-laptop.nix
@@ -20,6 +20,27 @@
     };
     hostName = "max-nixos-laptop";
   };
+  nix = {
+    buildMachines = [
+      {
+        hostName = "workstation.zandoodle.me.uk";
+        maxJobs = 16;
+        protocol = "ssh-ng";
+        sshUser = "nix-ssh";
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+        systems = [
+          "x86_64-linux" "armv7l-linux" "aarch64-linux" "mips-linux"
+          "mipsel-linux" "mips64-linux" "mips64el-linux" "riscv32-linux"
+          "riscv64-linux" "i686-linux"
+        ];
+      }
+    ];
+    distributedBuilds = true;
+    settings = {
+      avoid-local = true;
+      builders-use-substitutes = true;
+    };
+  };
   services = {
     logind.settings.Login.HandleLidSwitch = "ignore";
     xserver.videoDrivers = [ "nvidia" ];
--- chromebooksd2.nix
+++ chromebooksd2.nix
@@ -18,6 +18,27 @@
   networking = {
     hostName = "max-nixos-chromebooksd2";
   };
+  nix = {
+    buildMachines = [
+      {
+        hostName = "workstation.zandoodle.me.uk";
+        maxJobs = 16;
+        protocol = "ssh-ng";
+        sshUser = "nix-ssh";
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+        systems = [
+          "x86_64-linux" "armv7l-linux" "aarch64-linux" "mips-linux"
+          "mipsel-linux" "mips64-linux" "mips64el-linux" "riscv32-linux"
+          "riscv64-linux" "i686-linux"
+        ];
+      }
+    ];
+    distributedBuilds = true;
+    settings = {
+      avoid-local = true;
+      builders-use-substitutes = true;
+    };
+  };
   services.ip-mesh = {
     self = lib.mkForce "chromebook";
   };
